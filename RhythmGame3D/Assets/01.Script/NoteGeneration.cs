2775eb19e7a8b4ba4cdb4e5d40378176d430b9d4
pNoru <107230963+pNoru@users.noreply.github.com>
2023-06-08 03:49:22 +0900

    Added missing features and removed unnecessary ones.
    필요 없는 기능들을 제거하고 각 기능들의 최적화와 기능 추가 등을 하였습니다.
    
    Get help removing unneeded features, optimizing each feature, adding features, and more.

--- RhythmGame3D/Assets/01.Script/NoteGeneration.cs
+++ RhythmGame3D/Assets/01.Script/NoteGeneration.cs
@@ -2,29 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using Unity.VisualScripting;
 
 public class NoteGeneration : MonoBehaviour
 {
-    private int N, BPM;
+    private int N, BPM, Beat;
+    private bool start;
     private double CurrentTime;
     public GameObject Note;
     public GameObject LongNoteRail;
     private List<string> Data;
-    private string source, FilePath;
+    private string source, BBText;
+    public AudioManager AudioManager;
     private void Start()
     {
         N = 0;
-        FilePath = Application.dataPath + "/08.NSM/data.txt";
-        source = File.ReadAllText(FilePath).Replace("\n", string.Empty).Replace(" ", string.Empty); ;
+        start = false;
+        BBText = File.ReadAllText(Application.dataPath + "/08.NSM/bbdata.txt").Replace("\n", string.Empty).Replace(" ", string.Empty);
+        source = File.ReadAllText(Application.dataPath + "/08.NSM/data.txt").Replace("\n", string.Empty).Replace(" ", string.Empty);
+        Data = new List<string>(BBText.Split(","));
+        BPM = int.Parse(Data[0]); Beat = int.Parse(Data[1]);
         Data = new List<string>(source.Split(","));
     }
 
     private void Update()
     {
-        
+        if (Data[N] == string.Empty)
+        {
+            start = false;
+        }
+        if (start)
+        {
+            CurrentTime += Time.deltaTime;
+            
+            if(CurrentTime >= 60d / BPM / Beat)
+            {
+                Generation(Data.GetRange(N * 4, (N * 4) + 4));
+                CurrentTime -= 60d / BPM / Beat;
+                N += 1;
+            }
+        }
     }
 
-    void Generation(List<string> NG)
+    private void Generation(List<string> NG)
     {
         if (int.Parse(NG[0]) == 1)
         {
